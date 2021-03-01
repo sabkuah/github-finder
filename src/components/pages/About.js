68ebf171d9e2e835cd1a62276aa19087bcbae855
Sabrina Kuah <swkuah@gmail.com>
2021-02-28 23:46:38 -0800

    router added

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p>App to search GitHub Users</p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+        excepturi architecto eaque ipsam tempore animi molestiae incidunt quia
+        voluptatibus pariatur, est accusantium iste autem voluptate temporibus.
+        Accusantium quis nemo a?
+      </p>
+    </div>
+  );
+};
